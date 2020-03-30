2357d83dd9cac5f186abaf56471d11c391cb8a3c
Evgeniy Khyst <evgeniy.khist@gmail.com>
2020-03-30 20:56:05 +0300

    Fix directories

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -8,25 +8,24 @@ if [ -z "$DOMAINS" ]; then
 fi
 
 for domain in $DOMAINS; do
-  if [ ! -f "/etc/nginx/ssl/certs/$domain/fullchain.pem" ]; then
+  if [ ! -f "/etc/nginx/ssl/dummy/$domain/fullchain.pem" ]; then
     echo "Generating dummy ceritificate for $domain"
-    mkdir -p "/etc/nginx/ssl/certs/$domain"
+    mkdir -p "/etc/nginx/ssl/dummy/$domain"
     printf "[dn]\nCN=${domain}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${domain}, DNS:www.${domain}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" > openssl.cnf
-    openssl req -x509 -out "/etc/nginx/ssl/certs/$domain/fullchain.pem" -keyout "/etc/nginx/ssl/certs/$domain/privkey.pem" \
+    openssl req -x509 -out "/etc/nginx/ssl/dummy/$domain/fullchain.pem" -keyout "/etc/nginx/ssl/dummy/$domain/privkey.pem" \
       -newkey rsa:2048 -nodes -sha256 \
       -subj "/CN=${domain}" -extensions EXT -config openssl.cnf
     rm -f openssl.cnf
   fi
 done
 
-if [ ! -f /etc/nginx/ssl/dhparams/ssl-dhparams.pem ]; then
-  mkdir -p "/etc/nginx/ssl/dhparams"
-  openssl dhparam -out /etc/nginx/ssl/dhparams/ssl-dhparams.pem 2048
+if [ ! -f /etc/nginx/ssl/ssl-dhparams.pem ]; then
+  openssl dhparam -out /etc/nginx/ssl/ssl-dhparams.pem 2048
 fi
 
 use_lets_encrypt_certificates() {
   echo "Switching Nginx to use Let's Encrypt certificate for $1"
-  sed -i "s|/etc/nginx/ssl/certs/$1|/etc/letsencrypt/live/$1|g" /etc/nginx/conf.d/default.conf
+  sed -i "s|/etc/nginx/ssl/dummy/$1|/etc/letsencrypt/live/$1|g" /etc/nginx/conf.d/default.conf
 }
 
 reload_nginx() {
