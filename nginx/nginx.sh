641a02fe05af1f5d0a6e2e307eeef51120a0a76a
Evgeniy Khyst <evgeniy.khist@gmail.com>
2020-03-30 18:15:10 +0300

    Fix directories

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -8,18 +8,13 @@ if [ -z "$DOMAINS" ]; then
 fi
 
 for domain in $DOMAINS; do
-  if [ ! -f "/etc/letsencrypt/live/$domain/fullchain.pem" ]; then
+  if [ ! -f "/etc/nginx/ssl/$domain/fullchain.pem" ]; then
     mkdir -p "/etc/nginx/ssl/$domain"
-    
     printf "[dn]\nCN=${domain}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${domain}, DNS:www.${domain}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" > openssl.cnf
     openssl req -x509 -out "/etc/nginx/ssl/$domain/fullchain.pem" -keyout "/etc/nginx/ssl/$domain/privkey.pem" \
       -newkey rsa:2048 -nodes -sha256 \
       -subj "/CN=${domain}" -extensions EXT -config openssl.cnf
     rm -f openssl.cnf
-
-    mkdir -p "/etc/letsencrypt/live/$domain"
-    ln -s "/etc/nginx/ssl/$domain/fullchain.pem" "/etc/letsencrypt/live/$domain/fullchain.pem"
-    ln -s "/etc/nginx/ssl/$domain/privkey.pem" "/etc/letsencrypt/live/$domain/privkey.pem"
   fi
 done
 
@@ -27,6 +22,11 @@ if [ ! -f /etc/nginx/ssl/ssl-dhparams.pem ]; then
   openssl dhparam -out /etc/nginx/ssl/ssl-dhparams.pem 2048
 fi
 
-(sleep 30s && nginx -s reload)&
+for domain in $DOMAINS; do
+  if inotifywait -e close_write,moved_to "/etc/letsencrypt/live/$domain"; then
+    sed -i "s|/etc/nginx/ssl/$domain|/etc/letsencrypt/live/$domain|g" /etc/nginx/conf.d/default.conf
+    nginx -s reload
+  fi &
+done
 
 exec nginx -g "daemon off;"
