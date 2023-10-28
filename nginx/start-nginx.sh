769dfe65c62f1fd4b46db5c15ce29638761c6b2d
Eugene Khyst <evgeniy.khyst@gmail.com>
2023-10-28 11:34:18 +0300

    Fix #64 (#65)

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -2,15 +2,19 @@
 
 source ./config-nginx.sh
 
+nginx_dir="/etc/nginx"
 letsencrypt_certs_dir="/etc/letsencrypt"
 
-wait_for_lets_encrypt_certificate() {
-  until [ -d "${letsencrypt_certs_dir}/live/${1}" ]; do
+wait_for_lets_encrypt_certificate() {  
+  while [ -f "${nginx_dir}/conf.d/${1}.conf" ]; do
     echo "Waiting for Let's Encrypt certificate for $1"
     sleep 5s & wait ${!}
+    if [ -d "${letsencrypt_certs_dir}/live/${1}" ]; then
+      use_lets_encrypt_certificate "$1"
+      reload_nginx
+      break
+    fi
   done
-  use_lets_encrypt_certificate "$1"
-  reload_nginx
 }
 
 for domain in $domains; do
